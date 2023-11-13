0b07bdbe8e626eae7ccfc7e89dee42f239b82bd6
Arusu <125268704+Arusu12@users.noreply.github.com>
2023-11-13 23:31:14 +0600

    Upload

---
+++ script.ps1
@@ -0,0 +1,11 @@
+# Run npm install
+npm install
+
+# Run npm install electron globally
+npm install electron -g
+
+# Run Electron
+electron electron.js
+
+# Pause to keep the PowerShell window open
+Read-Host -Prompt "Press Enter to exit"
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = config.port;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
